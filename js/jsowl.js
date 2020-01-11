640bbce33aa7dd81cd30b6b85336f5a558027409
srabonosondha <59341023+srabonosondha@users.noreply.github.com>
2020-01-11 15:57:57 +0600

    Add files via upload

---
+++ js/jsowl.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
